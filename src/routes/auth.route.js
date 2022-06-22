c938faf1f4d47fb2fe11042087611be8a389fb80
buiphuong <buithiphuong07031999@gmail.com>
2022-06-22 15:00:05 +0700

    commit aa

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -12,4 +12,5 @@ router.get('/sendMail', authController.sendMail)
 router.post('/changePassword', authController.changePassword)
 router.get('/getListUser', authController.getListUser)
 
+
 module.exports = router
